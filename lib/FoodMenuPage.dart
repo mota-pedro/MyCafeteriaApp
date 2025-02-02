6903507cda89f555f76210711fce0eb6fb3d2d2f
Pedro Mota <pedro.rito@upe.br>
2025-02-02 14:30:41 -0300

    feat: new class Coffee

--- lib/FoodMenuPage.dart
+++ lib/FoodMenuPage.dart
@@ -7,8 +7,16 @@ class FoodMenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color(0xFFc3996c),      
-    );
+      color: Color(0xFFc3996c),
+      child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          children: [
+            
+          ],
+        ),
+      );
   }
 
 }
---
+++ lib/model/Coffee.dart
@@ -0,0 +1,9 @@
+class Coffee {
+
+  String name;
+  double value;
+  bool isHot;
+
+  Coffee({required this.name, required this.value, required this.isHot});
+
+}
